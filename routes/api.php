91e038e46eadd57170c3ff8a462d959ae554db39
Linh <thuylinh1@gmail.com>
2026-09-14 12:48:49 +0700

    main

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\FoodController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 Route::get('/health', function () {
     return response()->json([
@@ -18,3 +22,20 @@ Route::get('/foods', [FoodController::class, 'index']);
 Route::get('/foods/{id}', [FoodController::class, 'show']);
 Route::put('/foods/{id}', [FoodController::class, 'update']);
 Route::delete('/foods/{id}', [FoodController::class, 'destroy']);
+
+Route::get('/drinks', [ProductController::class, 'index']);
+Route::get('/drinks/{id}', [ProductController::class, 'show']);
+Route::put('/drinks/{id}', [ProductController::class, 'update']);
+Route::delete('/drinks/{id}', [ProductController::class, 'destroy']);
+
+Route::prefix('/category')->group(function(){
+    Route::get('/',[CategoryController::class, 'index']);
+    Route::post('/',[CategoryController::class, 'create']);
+});
+Route::prefix('/book')->group(function(){
+    Route::get('/', [BookController::class, 'index']);
+    Route::post('/', [BookController::class, 'create']);
+    Route::get('/{id}', [BookController::class, 'show']);
+    Route::put('/{id}', [BookController::class, 'update']);
+    Route::delete('/{id}', [BookController::class, 'delete']);
+});
